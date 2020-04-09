55a4840ef97f5780e4ec1fd946bf33b1aac2589f
roco <ryxn@ryxn.xyz>
2020-04-09 16:40:36 -0400

    Added Sorting and other stuff
    +code cleanup

--- covid19-trackerGUI/Views/MainWindow.xaml.cs
+++ covid19-trackerGUI/Views/MainWindow.xaml.cs
@@ -1,22 +1,14 @@
-﻿using System;
-using System.Collections;
+﻿using covid19_trackerGUI.ViewModels;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using covid19_trackerGUI.ViewModels;
-using Newtonsoft.Json;
-using RestSharp;
 
 namespace covid19_trackerGUI
 {
@@ -26,60 +18,62 @@ namespace covid19_trackerGUI
     public partial class MainWindow : Window
     {
         private RestClient _client = new RestClient();
-        private ObjectVm _vm = new ObjectVm();
-        private jsonParse.Tracker track = new jsonParse.Tracker();
-        private int allIndex;
-        private int _maxUpdateTime = 10;
+        private readonly ObjectVm _vm = new ObjectVm();
+        private jsonParse.Tracker _track = new jsonParse.Tracker();
+        private int _allIndex;
+        private const int MaxUpdateTime = 10;
 
         public MainWindow()
         {
             InitializeComponent();
             DataContext = _vm ?? new ObjectVm();
-            _vm.WorldwideVm = new BaseModel.Worldwide();
-            _vm.CountryVm = new List<BaseModel.Country>();
-            _vm.vm = new Base();
+            if (_vm != null)
+            {
+                _vm.WorldwideVm = new BaseModel.Worldwide();
+                _vm.CountryVm = new List<BaseModel.Country>();
+                _vm.Vm = new Base();
+            }
+
             PrepareData();
         }
 
         private void PrepareData()
         {
              ReadFromRawJsonFile();
-            foreach (var c in track.Response)
-            {
-                if (c.Country == "World" || c.Country=="All")
-                {
-                    if (c.Country != "World") continue;
-                    _vm.WorldwideVm.wwNewCases = c.Cases.New;
-                    _vm.WorldwideVm.wwActive = c.Cases.Active;
-                    _vm.WorldwideVm.wwCritical = c.Cases.Critical;
-                    _vm.WorldwideVm.wwRecovered = c.Cases.Recovered;
-                    _vm.WorldwideVm.wwTotalCases = c.Cases.Total;
-                    _vm.WorldwideVm.wwNewDeaths = c.Deaths.New;
-                    _vm.WorldwideVm.wwTotalDeaths = c.Deaths.Total;
-                }else
-                    _vm.CountryVm.Add(new BaseModel.Country() { Name = c.Country , NewCases = c.Cases.New , Active = c.Cases.Active, Critical = c.Cases.Critical, Recovered = c.Cases.Recovered, TotalCases = c.Cases.Total, NewDeaths = c.Deaths.New, TotalDeaths = c.Deaths.Total});
-            }
-            BindData();
-            UpdateTimer();
+             _track.Response.Sort(((response, response1) => response1.Cases.Total.CompareTo(response.Cases.Total)));
+             foreach (var c in _track.Response)
+             {
+                 if (c.Country == "World" || c.Country=="All")
+                 {
+                     _vm.WorldwideVm.wwNewCases = c.Cases.New;
+                     _vm.WorldwideVm.wwActive = c.Cases.Active;
+                     _vm.WorldwideVm.wwCritical = c.Cases.Critical;
+                     _vm.WorldwideVm.wwRecovered = c.Cases.Recovered;
+                     _vm.WorldwideVm.wwTotalCases = c.Cases.Total;
+                     _vm.WorldwideVm.wwNewDeaths = c.Deaths.New;
+                     _vm.WorldwideVm.wwTotalDeaths = c.Deaths.Total;
+                 }else
+                     _vm.CountryVm.Add(new BaseModel.Country() { Name = c.Country , NewCases = c.Cases.New , Active = c.Cases.Active, Critical = c.Cases.Critical, Recovered = c.Cases.Recovered, TotalCases = c.Cases.Total, NewDeaths = c.Deaths.New, TotalDeaths = c.Deaths.Total});
+             }
+             BindData();
+             Task.Run(UpdateTimer);
         }
 
         //TODO: Fix UpdateTimer,UpdateData,ApiUpdateData to use TASK instead of method 
         //BUG: DATA DOESN'T UPDATE IN UI AFTER BEING UPDATED HERE USING TASKS DOESN'T HELP EITHER
 
-        private async void UpdateTimer()
+        private async Task UpdateTimer()
         {
             while (true)
             {
-                var ts = DateTime.Now - _vm.vm.UpDateTime;
-                if (!(ts.TotalSeconds >= _maxUpdateTime))
+                var ts = DateTime.Now - _vm.Vm.UpDateTime;
+                if (!(ts.TotalSeconds >= MaxUpdateTime))
                 {
                     await Task.Delay(500); continue;}
                 ApiUpdateData();
                 UpdateData();
-                _vm.vm.UpDateTime = DateTime.Now;
+                _vm.Vm.UpDateTime = DateTime.Now;
             }
-
-
         }
         private void BindData()
         {
@@ -89,21 +83,48 @@ namespace covid19_trackerGUI
             TotalCases.DataContext = _vm.WorldwideVm;
             TotalDeaths.DataContext = _vm.WorldwideVm;
             TotalRecoveries.DataContext = _vm.WorldwideVm;
-            Updatetime.DataContext = _vm.vm;
+            Updatetime.DataContext = _vm.Vm;
             selectedCountry.DataContext = _vm.CountryVm[0];
             selectedCountryExtra.DataContext = _vm.CountryVm[0];
             SelectedCountryInfo.DataContext = _vm.CountryVm[0];
         }
+
         private void ReadFromRawJsonFile()
         {
-            _vm.vm.UpDateTime = File.GetLastWriteTime("AllInfectedCountries.json");
-            var jsonraw = File.ReadAllText("AllInfectedCountries.json");
-            track = JsonConvert.DeserializeObject<jsonParse.Tracker>(jsonraw);
+            while (true)
+            {
+                if (File.Exists("AllInfectedCountries.json"))
+                {
+                    if (new FileInfo("AllInfectedCountries.json").Length != 0)
+                    {
+                        _vm.Vm.UpDateTime = File.GetLastWriteTime("AllInfectedCountries.json");
+                        var jsonraw = File.ReadAllText("AllInfectedCountries.json");
+                        _track = JsonConvert.DeserializeObject<jsonParse.Tracker>(jsonraw);
+                       
+                    }
+                    else
+                    {
+                        ApiUpdateData();
+                        continue;
+                    }
+                }
+                else 
+                {
+                    var fc = File.Create("AllInfectedCountries.json");
+                    fc.Close();
+                    ApiUpdateData();
+                    continue;
+                }
+
+                break;
+            }
         }
+
         private void UpdateData()
         {
-            allIndex = track.Response.FindIndex(x => x.Country == "All");
-            foreach (var c in track.Response.Where(x => !x.Country.Equals("All") && !x.Country.Equals("World")))
+            _allIndex = _track.Response.FindIndex(x => x.Country == "All");
+            Updatetime.Text = DateTime.Now.AddHours(1).ToString("G");
+            foreach (var c in _track.Response.Where(x => !x.Country.Equals("All") && !x.Country.Equals("World")))
             {
                 var tempindex = _vm.CountryVm.FindIndex(x => x.Name == c.Country);
                 _vm.CountryVm[tempindex].NewCases = c.Cases.New;
@@ -122,7 +143,7 @@ namespace covid19_trackerGUI
             request.AddHeader("x-rapidapi-host", "covid-193.p.rapidapi.com");
             request.AddHeader("x-rapidapi-key", "db654bb9eemshe5a8718dd1418ffp1b94abjsna0a6321ba60a");
             var response = _client.Execute(request);
-            track = JsonConvert.DeserializeObject<jsonParse.Tracker>(response.Content);
+            _track = JsonConvert.DeserializeObject<jsonParse.Tracker>(response.Content);
             File.WriteAllText(@"./AllInfectedCountries.json", response.Content);
         }
 
@@ -130,16 +151,16 @@ namespace covid19_trackerGUI
         {
             var item = ((ListView) sender).SelectedItem;
             if (item == null) return;
-            var itemID = cList.Items.IndexOf(item);
-            selectedCountry.DataContext = _vm.CountryVm[itemID];
-            selectedCountryExtra.DataContext = _vm.CountryVm[itemID];
-            SelectedCountryInfo.DataContext = _vm.CountryVm[itemID];
+            var itemId = cList.Items.IndexOf(item);
+            selectedCountry.DataContext = _vm.CountryVm[itemId];
+            selectedCountryExtra.DataContext = _vm.CountryVm[itemId];
+            SelectedCountryInfo.DataContext = _vm.CountryVm[itemId];
         }
     }
 
     public class ObjectVm
     {
-        public Base vm { get; set; }
+        public Base Vm { get; set; }
         public List<BaseModel.Country> CountryVm { get; set; }
         public BaseModel.Worldwide WorldwideVm { get; set; }
     }
